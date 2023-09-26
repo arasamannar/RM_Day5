f267f786f016218d2f42ff067e21f9413ce3f5eb
arasamannar <arasamannar@gmail.com>
2023-09-26 20:48:29 +0530

    Added the resume.js file containing the resume of mine as an JSON object

---
+++ resume.js
@@ -0,0 +1,15 @@
+let res= {
+    "name": "Arasamannar A K",
+    "Industry": "IT",
+    "Experience": 5,
+    "Role": "Senior Analyst",
+    "Work Location": "Chennai",
+    "Education": "B.E-EEE",
+    "Summary": `SharePoint Administrator with 5+ years of experience in managing and supporting SharePoint environments.
+    Proven ability to install, configure, maintain, and troubleshoot SharePoint servers and sites.
+    Expertise in SharePoint security, permissions, and auditing.
+    Strong customer service and communication skills.`,
+    "certifications": `1. Microsoft Certified Solutions Associate (MCSA): SharePoint Server 2016
+    2. Microsoft Certified Solutions Expert (MCSE): SharePoint Server 2016`
+}
+console.log(res);
